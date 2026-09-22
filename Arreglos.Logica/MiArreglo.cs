647750e558475d424dc8442d5080322903f124ef
DanHee-15 <25030021@itesa.edu.mx>
2026-09-22 08:27:23 -0600

    Versión_02: Proyectos en blanco.

---
+++ Arreglos.Logica/MiArreglo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arreglos.Logica
+{
+    public class MiArreglo
+    {
+    }
+}
